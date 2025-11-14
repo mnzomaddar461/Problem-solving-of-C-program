e87d44085e9a76d2a7ec3f3488c4406b4f774ec7
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2025-11-14 14:33:56 +0600

    Contest

---
+++ Week 05/Contest Magical Tree.c
@@ -0,0 +1,380 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    if(n == 1){
+        int rows = 6;        
+        int star = 1;
+        int maxStars = 11;
+
+        for(int i = 0; i < rows; i++){
+            int space = (maxStars - star)/2;
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+        int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+    }
+
+    if(n == 3 ){
+        int row = 7;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+        int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+        if(n == 5){
+        int row = 8;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+
+        if(n == 7){
+        int row = 9;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+        if(n == 9 ){
+        int row = 10;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+        if(n == 11){
+        int row = 11;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+            if(n == 13){
+        int row = 12;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+        
+    }
+
+                if(n == 15){
+        int row = 13;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+        
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+    }
+
+                if(n == 17){
+        int row = 14;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+        
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+    }
+
+                if(n == 19){
+        int row = 15;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+        
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+    }
+
+                if(n == 21){
+        int row = 16;
+        int star = 1;
+        int space = row - 1;
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < space; j++){
+                printf(" ");
+            }
+            for(int k = 0; k < star; k++){
+                printf("*");
+            }
+            printf("\n");
+            star += 2;
+            space--;
+        }
+        
+                int row_root = 5;
+        int col_root = n;
+        int space_root = 5;
+        //int star_root = 1;
+
+        for(int i_root = 0; i_root < row_root; i_root++){
+            for(int j_root = 0; j_root < space_root; j_root++){
+                printf(" ");
+            }
+            for(int k_root = 0; k_root < col_root; k_root++){
+                printf("*");
+            }
+            printf("\n");
+            
+        }
+    }
+
+    return 0;
+}
